a59098c2909967035c74eb1333ed9156f9977426
siddharth905 <siddharthkalamkar905@gmail.com>
2024-02-11 13:01:09 +0530

    Sucessfully connected with Mongodb database

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ const app=express();
 require("dotenv").config();
 const PORT=process.env.PORT || 3000
 
+//middleWare
+app.use(express.json());
+
+//Connection with MongoDb Database
+const connectWithDb=require("./config/database");
+connectWithDb();
+
+//start the server
+app.listen(PORT,()=>{
+    console.log(`App is started at port no ${PORT}`);
+})
+
+//Default
+app.get("/",()=>{
+    res.send(`<h1>This is HomePage</h1>`);
+})
+
 
